002daec81876019f7a12cb71cfb67954075431cb
Cloud327 <roman.ermolaev@hotmail.com>
2022-04-19 12:03:21 +0200

    ,

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,34 @@
-const WINDOW_TITLE: &str = "Hello Window";
-use beryllium::*;
+use kiss3d::nalgebra as na;
+
+use kiss3d::light::Light;
+use kiss3d::resource::Mesh;
+use kiss3d::window::Window;
+use na::{Point3, UnitQuaternion, Vector3};
+use std::cell::RefCell;
+use std::rc::Rc;
 
 fn main() {
-    let sdl = SDL::init(InitFlags::Everything).expect("couldn't start SDL");
-    sdl.gl_set_attribute(SdlGlAttr::MajorVersion, 3).unwrap();
-    sdl.gl_set_attribute(SdlGlAttr::MinorVersion, 3).unwrap();
-    sdl.gl_set_attribute(SdlGlAttr::Profile, GlProfile::Core).unwrap();
-  
+    let mut window = Window::new("Kiss3d: custom_mesh");
 
-    let _win = sdl
-    .create_gl_window(
-      WINDOW_TITLE,
-      WindowPosition::Centered,
-      800,
-      600,
-      WindowFlags::Shown,
-    )
-    .expect("couldn't make a window and context");
+    let a = Point3::new(-1.0, -1.0, 0.0);
+    let b = Point3::new(1.0, -1.0, 0.0);
+    let c = Point3::new(0.0, 1.0, 0.0);
+
+    let vertices = vec![a, b, c];
+    let indices = vec![Point3::new(0u16, 1, 2)];
+
+    let mesh =Rc::new(RefCell::new(Mesh::new(vertices, indices, None, None, false)));
+    let mut c = window.add_mesh(mesh, Vector3::new(1.0, 1.0, 1.0));
+    c.set_color(1.0, 0.0, 0.0);
+    c.enable_backface_culling(false);
+
+    window.set_light(Light::StickToCamera);
+
+    //let rot = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), 0.014);
 
 
-    'main_loop: loop {
-        // handle events this frame
-        while let Some(event) = sdl.poll_events().and_then(Result::ok) {
-          match event {
-            Event::Quit(_) => break 'main_loop,
-            _ => (),
-          }
-        }
-        // now the events are clear
-    
-        // here's where we could change the world state and draw.
-      }
+    while window.render() {
+        
+       //c;
+    }
 }
-
-
-
-
